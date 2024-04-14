3f8dcd98a9323f26ebef08447b776b05ec1fd23a
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-04-14 18:48:57 -0300

    utimo

---
+++ Introdução ao JavaScript/ex4/laçoaninhado.js
@@ -0,0 +1,14 @@
+for (A = 1; A <= 5; A++) {   //Primeiro laço
+    for (B = 1; B <= 3; B++) {  //Segundo Laço
+        console.log(`Incremento de 1 para B ${B}`)
+    }
+    console.log(`Incremento de 1 para A ${A}`)
+}
+console.log('Loping Finalizado')
+
+var js_contador=1;
+ 
+while (js_contador <= 10) { 
+   console.log('8 X ' + js_contador + ' = ' + 8*js_contador); 
+   js_contador +=1;
+}
